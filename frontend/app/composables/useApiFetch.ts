e7879712168d801aa60b9409db492304a7165d64
Ivan <nikk452@gmail.com>
2026-07-11 15:51:08 +0300

    Update `useApiFetch` to set unique key based on request string

--- frontend/app/composables/useApiFetch.ts
+++ frontend/app/composables/useApiFetch.ts
@@ -2,8 +2,10 @@ import type { UseFetchOptions } from 'nuxt/app'
 
 export const useApiFetch = <T>(request: string | (() => string), opts?: UseFetchOptions<T>) => {
   const config = useRuntimeConfig()
+  const reqStr = typeof request === 'string' ? request : request()
   
   return useFetch<T>(request, {
+    key: opts?.key || reqStr,
     baseURL: import.meta.server ? config.apiLocal : config.public.apiBase,
     ...opts
   } as any)
